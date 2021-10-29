70f4287ae7da542d1cf6ba57c62ccd2e508d9595
Leo Perrouault <perrouault@gmail.com>
2021-10-29 11:34:43 +0200

    affichage DB

--- src/Controller/NewsController.php
+++ src/Controller/NewsController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Service\DataBaseFunction;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,8 @@ use Symfony\Component\Routing\Annotation\Route;
 class NewsController extends AbstractController
 {
     #[Route('/news', name: 'news')]
-    public function index(): Response
-    {
-        return $this->render('news/index.html.twig', [
-            'controller_name' => 'NewsController',
-        ]);
+    public function index(DataBaseFunction $baseFunction): Response{
+        $articles = $baseFunction->fetchingObjectAll(Article::class);
+        return $this->render('news/index.html.twig', ['articles' => $articles,]);
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ArticleRepository;
+use App\Entity\tag;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
