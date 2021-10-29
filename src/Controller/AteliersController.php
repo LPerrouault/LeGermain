6f0750004144c67a6b98f2abf27f336594d1f4b5
Yukkin <nng_sphie@hotmail.com>
2021-10-29 12:16:22 +0200

    affichage bd (ateliers)

--- src/Controller/AteliersController.php
+++ src/Controller/AteliersController.php
@@ -5,14 +5,29 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Atelier;
 
 class AteliersController extends AbstractController
 {
     #[Route('/ateliers', name: 'ateliers')]
-    public function index(): Response
-    {
+    public function index(): Response{
+
+        $repo = $this->getDoctrine()->getRepository(Atelier::class);
+        $ateliers = $repo->findAll();
+
         return $this->render('ateliers/index.html.twig', [
-            'controller_name' => 'AteliersController',
+                'controller_name' => 'AteliersController',
+                'ateliers' => $ateliers
         ]);
     }
+
+    /**
+    public function index(): Response
+    {
+    return $this->render('ateliers/index.html.twig', [
+    'controller_name' => 'AteliersController',
+    ]);
+    }
+     */
+
 }
