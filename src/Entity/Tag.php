b9326307a9a946da38efbfcc515e017102d74978
Leo Perrouault <perrouault@gmail.com>
2021-11-16 20:20:41 +0100

    hotfix Tag et Type

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=TagRepository::class)
  */
-class tag
+class Tag
 {
     /**
      * @ORM\Id
--- src/Entity/Type.php
+++ src/Entity/Type.php
@@ -8,7 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
 /**
  * @ORM\Entity(repositoryClass=TypeRepository::class)
  */
-class type
+class Type
 {
     /**
      * @ORM\Id
